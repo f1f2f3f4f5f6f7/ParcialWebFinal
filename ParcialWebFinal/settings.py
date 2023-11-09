1e2d7f71ae4dc5cbf4fb438fd9e342f183cc84d6
Kevin C <castrokevin312@gmail.com>
2023-11-09 15:40:51 -0500

    agg_Aplicaciones

--- ParcialWebFinal/settings.py
+++ ParcialWebFinal/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'foros.apps.ForosConfig',
+    'noticias.apps.NoticiasConfig',
+    'commons.apps.CommonsConfig'
 ]
 
 MIDDLEWARE = [
